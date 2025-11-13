8e66600cc44e43510d583a4cf24d7bfd800be57c
ShaikhHamza104 <kmohdhamza10@gmail.com>
2025-11-13 14:33:58 +0530

    Update main.py add port

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ def predict():
         return render_template('index.html', prediction_text=f"Error: {e}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
